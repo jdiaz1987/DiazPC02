526d974d2582ce261511f386f448ec4d40678a5c
jdiaz1987 <95186690+jdiaz1987@users.noreply.github.com>
2022-10-08 01:42:10 -0500

    Agregar archivos de proyecto.

---
+++ DiazPC02.DOMAIN/Infrastructure/Repositories/SupplierRepository.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DiazPC02.DOMAIN.Core.Entities;
+using DiazPC02.DOMAIN.Infrastructure.Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace DiazPC02.DOMAIN.Infrastructure.Repositories
+{
+    public class SupplierRepository
+    {
+        private readonly SalesContext _context;
+
+        public SupplierRepository(SalesContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IEnumerable<Supplier>> GetSuppliers()
+        {
+            var suppliers = await _context.Supplier.ToListAsync();
+            return suppliers;
+        }
+
+        public async Task<Supplier> GetSupplier(int id)
+        {
+            return await _context.Supplier.Where(x => x.Id == id).FirstOrDefaultAsync();
+        }
+
+        public async Task<IEnumerable<Supplier>> GetSupplierByCountry(string country)
+        {
+            var suppliers = await _context.Supplier.Where(x => x.Country == country).ToListAsync();
+            return suppliers;
+        }
+
+        public async Task<bool> Insert(Supplier supplier)
+        {
+            await _context.Supplier.AddAsync(supplier);
+            var countRows = await _context.SaveChangesAsync();
+            return (countRows > 0);
+        }
+
+        public async Task<bool> Update(Supplier supplier)
+        {
+            _context.Supplier.Update(supplier);
+            var countRows = await _context.SaveChangesAsync();
+            return (countRows > 0);
+        }
+
+        public async Task<bool> Delete(int id)
+        {
+            var supplier = await _context.Supplier.FindAsync(id);
+            if (supplier == null)
+                return false;
+            _context.Supplier.Remove(supplier);
+            var countRows = await _context.SaveChangesAsync();
+            return (countRows > 0);
+        }
+
+    }
+}
