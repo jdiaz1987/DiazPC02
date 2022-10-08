9464b9d026b3455c86374332db1339fc9ec83fae
jdiaz1987 <95186690+jdiaz1987@users.noreply.github.com>
2022-10-08 02:41:41 -0500

    Finish CRUD supplier controller

--- DiazPC02.API/Controllers/SupplierController.cs
+++ DiazPC02.API/Controllers/SupplierController.cs
@@ -1,4 +1,5 @@
-﻿using DiazPC02.DOMAIN.Core.Interfaces;
+﻿using DiazPC02.DOMAIN.Core.Entities;
+using DiazPC02.DOMAIN.Core.Interfaces;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
@@ -22,5 +23,42 @@ namespace DiazPC02.API.Controllers
             return Ok(suppliers);
         }
 
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetSupplierById(int id)
+        {
+            var supplier = await _supplierRepository.GetSupplier(id);
+            return Ok(supplier);
+        }
+
+        [HttpGet("GetByCountry")]
+        public async Task<IActionResult> GetByCountry([FromQuery]string country)
+        {
+            var supplier = await _supplierRepository.GetSupplierByCountry(country);
+            return Ok(supplier);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Insert([FromBody]Supplier supplier)
+        {
+            var result = await _supplierRepository.Insert(supplier);
+            return Ok(result);
+        }
+
+        [HttpPut]
+        public async Task<IActionResult> Update(int id, [FromBody] Supplier supplier)
+        {
+            if (id != supplier.Id)
+                return BadRequest();
+
+            var result = await _supplierRepository.Update(supplier);
+            return Ok(result);
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> Delete(int id)
+        {
+            var result = await _supplierRepository.Delete(id);
+            return Ok(result);
+        }
     }
 }
